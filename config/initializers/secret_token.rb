34fb339c1fed1b27367bc0ae0dcff28171d8007d
josh <joshearl1@hotmail.com>
2012-08-27 12:50:36 -0400

    First commit of my genome comparison database

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GenomeComparison::Application.config.secret_token = '3d6c6b71ef737981623b6da668b542c426c1810a9e61a0e674363e0f10c9f9c1b6905082c93e8a2387802338b8d3225232fac05f2a3f05453bd0966e0aa11789'
